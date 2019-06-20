fd1027679717e6bb2cb0dca172a2ab75af7cdf76
renbuzhudek <464879673@qq.com>
2019-06-20 23:18:12 +0800

    1

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ import './util/rem'
 import ElementUI from 'element-ui';
 
 import 'element-ui/lib/theme-chalk/index.css';
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 Vue.use(ElementUI)
 Vue.use(VueRx, Rx)
 /* eslint-disable no-new */
