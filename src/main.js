97aaf92a6676445825ec855b762efa1a33e4270d
renbuzhudek <464879673@qq.com>
2019-06-20 23:15:45 +0800

    1

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ import './util/rem'
 // Vue.use(Mint);
 import ElementUI from 'element-ui';
 
-import 'element-ui/lib/theme-chalk/index.css'
+import 'element-ui/lib/theme-chalk/index.css';
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 Vue.use(VueRx, Rx)
