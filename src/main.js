5a04d1976ac984d22a6875adcd46d7e96ede6cc9
renbuzhudek <464879673@qq.com>
2019-06-20 23:14:37 +0800

    1

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ import './util/rem'
 // import Mint from 'mint-ui';
 // import 'mint-ui/lib/style.css';
 // Vue.use(Mint);
-import ElementUI from 'element-ui'
+import ElementUI from 'element-ui';
 
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
