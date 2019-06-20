9f17811b2c245f76d465031d5beee2c7732f94af
renbuzhudek <464879673@qq.com>
2019-06-20 23:27:41 +0800

    1

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import store from './store'
+import store from './store';
 import VueRx from 'vue-rx';
 import Rx from 'rxjs/Rx';
 import './util/rem';
